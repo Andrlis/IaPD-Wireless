63e31d133d206edd45379ff51763c0f86e680693
Andrey Lisovskiy <andrlis0506@gmail.com>
2017-12-08 02:45:04 +0300

    Add wireless manager.

---
+++ WirelessManager/Resources/NetworkManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using SimpleWifi;
+using WirelessManager.Bean;
+
+namespace WirelessManager.Resources
+{
+    class NetworkManager
+    {
+        static public List<NetworkConnection> GetNetworkConnectionList()
+        {
+            List<NetworkConnection> answer = new List<NetworkConnection>();
+
+            string[] bssids = GetBssids();
+
+            try
+            {
+                Wifi wifi = new Wifi();
+
+                foreach (var accessPoint in wifi.GetAccessPoints())
+                {
+                    answer.Add(new NetworkConnection(accessPoint, GetMacAddress(bssids, accessPoint)));
+                }
+            }
+            catch (System.ComponentModel.Win32Exception)
+            {
+                answer = null;
+            }
+
+            return answer;
+        }
+
+        static private string[] GetBssids()
+        {
+            string[] bssids;
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    CreateNoWindow = true,
+                    FileName = "cmd",
+                    Arguments = @"/C ""netsh wlan show networks mode=bssid | findstr SSID""",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                }
+            };
+            process.Start();
+            bssids = process.StandardOutput.ReadToEnd().Replace(" ", "").Replace("\r", "").Split('\n');
+            process.WaitForExit();
+
+            return bssids;
+        }
+
+        static private string GetMacAddress(string[] bssids, AccessPoint accessPoint)
+        {
+            for (int i = 0; i < bssids.Length; i++)
+            {
+                if ((bssids[i].Split(':')[0].Contains("SSID") && accessPoint.Name.Equals(bssids[i].Split(':')[1])))
+                {
+                    return bssids[i + 1].Remove(0, bssids[i + 1].IndexOf(":") + 1);
+                }
+            }
+            return null;
+        }
+
+        static public bool Equals(List<NetworkConnection> a, List<NetworkConnection> b)
+        {
+            if (a == null || b == null || a.Count != b.Count)
+                return false;
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (!a[i].Equals(b[i]))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ WirelessManager/Resources/UpdateInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Management;
+using System.Windows.Forms;
+
+namespace WirelessManager.Resources
+{
+    class UpdateInfo
+    {
+        private Timer Timer;
+
+        public UpdateInfo()
+        {
+            Timer = new Timer
+            {
+                Interval = (10000)
+            };
+        }
+
+        public void AddListerenToTimer(EventHandler handler)
+        {
+            Timer.Tick += handler;
+        }
+
+        public void Start()
+        {
+            Timer.Start();
+        }
+    }
+}
